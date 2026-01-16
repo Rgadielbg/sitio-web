b92f169508cd24b091ca63a0496dfddbd5dea1c2
Rgadielbg <240349@utxicotepec.edu.mx>
2026-01-16 09:55:01 -0600

    nuevo2

--- JS/function.js
+++ JS/function.js
@@ -1,3 +1,29 @@
-function mosrtrarAlerta(){
-    alert("Hola bienvenido al sitio web");
+function mostrarAlerta(){
+    alert("Hola, bienvenido al sitio web")
+    console.info("Ejemplo de function");
+}
+function suma(){
+    let valor1 = 5;
+    let valor2 = 10; 
+    let resultado = valor1 + valor2;
+    console.info("El resultado de la suma es: "+resultado);
+}
+function sumaParam(valor1, valor2){
+    console.info("El valor 1: "+valor1);
+    console.info("E valor 2: "+valor2);
+
+}
+function sumarImput(){
+    let val1 = document.getElementById("val1").value;
+    let val2 = document.getElementById("val2").value;
+    let res = parseFloat(val1) + parseFloat(val2);
+    console.info("El resultado de la suma es: "+res);
+    if(res>10){
+        document.getElementById("mensaje").value="Numero mayor a 10";
+    }else{
+        document.getElementById("mensaje").value="Numero menor";
+    }
+}
+function sumarNumeros(){
+let 
 }
